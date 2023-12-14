cf5976529b5a1e910df7f33a4cccd71488c58817
Yusuf Gökhan Kuyu <yusufgokhankuyu@gmail.com>
2023-12-14 20:16:32 +0300

    About Page content is added

--- lib/screens/about_page.dart
+++ lib/screens/about_page.dart
@@ -27,21 +27,74 @@ class AboutPage extends StatelessWidget {
               color: Colors.red, fontSize: 24, fontWeight: FontWeight.bold),
         ),
       ),
-      body: Center(
+      body: const SingleChildScrollView(
+        child: Center(
           child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Icon(Icons.description, color: Colors.red, size: 32),
-              Text(
-                "Recipe App with Flutter",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.description, color: Colors.red, size: 32),
+                  Text(
+                    "Recipe App with Flutter",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Text(
+                      "Welcome to Recipe App, your ultimate culinary companion in the world of delightful recipes! We believe that cooking is an art, and our mission is to inspire and empower you to create mouthwatering dishes right in your own kitchen.",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Our Passion",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "At Recipe App, we are passionate about making the cooking experience enjoyable, accessible, and rewarding. Whether you're a seasoned chef or a novice in the kitchen, our app is designed to cater to your culinary needs, providing a diverse range of recipes that suit all tastes and skill levels.",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Get in Touch",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Have a question, suggestion, or just want to say hello? We'd love to hear from you! Reach out to us through [contact information] or connect with us on [social media platforms].Recipe App Team",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
               )
             ],
-          )
-        ],
-      )),
+          ),
+        ),
+      ),
       bottomNavigationBar: const MyBottomNavigationBar(),
     );
   }
